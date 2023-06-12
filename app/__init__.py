8885721d6461afdff63a25dc64ed602073ede98a
Agershkovich30 <90231118+Agershkovich30@users.noreply.github.com>
2023-06-12 10:37:32 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for, abort
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
