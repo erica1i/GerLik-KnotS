ec98753976a4bdaca83b4eaed6d9ba74f6cb29b9
Kosta Dubovskiy <kostadubovskiy@gmail.com>
2023-06-12 22:31:41 -0400

    merging

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,50 +1,71 @@
-fetch('/expenses_by_category')
-  .then(response => response.json())
-  .then(data => 
-    {
-    // `data` is the JSON data returned by the server
-    // You can use this data to create your chart
+// fetch('/expenses_by_category')
+//   .then(response => response.json())
+//   .then(data => 
+//     {
+//     const ctx = document.getElementById('expenseChart').getContext('2d');
+//     const labels = Object.keys(data);
+//     const values = Object.values(data);
 
-    const ctx = document.getElementById('expenseChart').getContext('2d');
-    const labels = Object.keys(data);
-    const values = Object.values(data);
+//     new Chart(ctx, {
+//       type: 'pie',
+//       data: {
+//         labels: labels,
+//         datasets: [{
+//           data: values,
+//           backgroundColor: [
+//             'rgba(255, 99, 132, 0.2)',
+//             'rgba(54, 162, 235, 0.2)',
+//             'rgba(255, 206, 86, 0.2)',
+//             'rgba(75, 192, 192, 0.2)',
+//             'rgba(153, 102, 255, 0.2)',
+//             'rgba(255, 159, 64, 0.2)'
+//           ],
+//           borderColor: [
+//             'rgba(255, 99, 132, 1)',
+//             'rgba(54, 162, 235, 1)',
+//             'rgba(255, 206, 86, 1)',
+//             'rgba(75, 192, 192, 1)',
+//             'rgba(153, 102, 255, 1)',
+//             'rgba(255, 159, 64, 1)'
+//           ],
+//           borderWidth: 1
+//         }]
+//       },
+//       options: {
+//         responsive: true,
+//         plugins: {
+//           legend: {
+//             position: 'top',
+//           },
+//           title: {
+//             display: true,
+//             text: 'Expenses by Category'
+//           }
+//         }
+//       }
+//     });
+//   });
 
-    new Chart(ctx, {
-      type: 'pie',
-      data: {
-        labels: labels,
-        datasets: [{
-          data: values,
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        responsive: true,
-        plugins: {
-          legend: {
-            position: 'top',
-          },
-          title: {
-            display: true,
-            text: 'Expenses by Category'
-          }
+function renderExpenseChart(categories, expenses) {
+  var ctx = document.getElementById('expenseChart').getContext('2d');
+  var chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: categories,
+      datasets: [{
+        label: 'Expenses',
+        data: expenses,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
       }
-    });
+    }
   });
+}
